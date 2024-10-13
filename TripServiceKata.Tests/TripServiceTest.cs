8ce2fb3ccf3fe9bf178e213ad87d7951ebc6134c
Belinda Hartl <152974825+belindahartl@users.noreply.github.com>
2024-10-13 17:59:21 +0200

    Added Tests get friends - Belinda

--- TripServiceKata.Tests/TripServiceTest.cs
+++ TripServiceKata.Tests/TripServiceTest.cs
@@ -1,6 +1,26 @@
-﻿namespace TripServiceKata.Tests
+﻿using System;
+using System.Collections.Generic;
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
+        [Fact]
+        public void TestGetFriends()
+        {
+            //ARRANGE setup
+            var user = new User.User();
+
+            //ACT die durchführung der getesteten Methode
+            var listFriends = user.GetFriends();
+
+            //ASSERTS abfrage
+            Assert.NotNull(listFriends);
+            Assert.Empty(listFriends);
+            Assert.IsType<List<User.User>>(listFriends);
+        }
     }
 }
--- TripServiceKata.Tests/obj/Debug/netcoreapp3.1/TripServiceKata.Tests.AssemblyInfo.cs
+++ TripServiceKata.Tests/obj/Debug/netcoreapp3.1/TripServiceKata.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TripServiceKata.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8d0d4c6423e873fc4dde8fbed58a63d0a4600a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TripServiceKata.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TripServiceKata.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
